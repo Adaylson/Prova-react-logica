02f86e901671ef104809cc8c8519808e0f7b712b
Adaylson Arcanjo <adaylsonevaristo15@gmail.com>
2022-10-07 11:42:44 -0300

    a

--- src/pages/prova1/index.js
+++ src/pages/prova1/index.js
@@ -7,7 +7,13 @@ export default function Prova1(){
     const [resp, setResp] = useState('')
 
     function Libras(){
-        if(mes === 'setembro' && dia>=23|| mes === 'outubro' && dia<=22 ){
+        if(dia <= 0 || dia > 31){
+            setResp('Dia invalido')
+        }
+        else if(!mes){
+            setResp('Insira o mes')
+        }          
+        else if(mes === 'setembro' && dia>=23|| mes === 'outubro' && dia<=22 ){
             setResp('Seu signo é de libras')
         }
         else{
@@ -20,6 +26,19 @@ export default function Prova1(){
             <div>
                 <div>
                     <h3> Libras ?? </h3>
+                    <select onChange={e => setMes(e.target.value)}>
+                        <option ></option>
+                        <option value={mes}></option>
+                        <option value={mes}></option>
+                        <option value={mes}></option>
+                        <option value={mes}></option>
+                        <option value={mes}></option>
+                        <option value={mes}></option>
+                        <option value={mes}></option>
+                        <option value={mes}></option>
+                        <option value={mes}></option>
+                        <option value={mes}></option>
+                    </select>
                     <input type='text' placeholder="mes de nascimento" value={mes} onChange={e => setMes(e.target.value)}/>
                     <input type='number' placeholder="dia de nascimento"  value={dia} onChange={e => setDia(Number(e.target.value))} />
 
