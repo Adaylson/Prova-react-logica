afbbff5e84cba1a305d54e4365d64eb6287fa0d8
Adaylson Arcanjo <adaylsonevaristo15@gmail.com>
2022-10-11 09:43:02 -0300

    kg

--- src/pages/prova5/index.js
+++ src/pages/prova5/index.js
@@ -7,13 +7,20 @@ export default function Prova5() {
     const [distancia, setDistancia] = useState(0);
     const [resp, setResp] = useState('');
 
+    function Paradas() {
+        let a = distancia / (tanque * consumo);
+        setResp(Math.ceil(a));
+    }
+
     return(
-        <main>
-            <div>
-                <div>
-                    <input type="number" onChange={e =>(setTanque(Number(e.target.value)))} />
-                    <input type="number" onChange={e =>(setConsumo(Number(e.target.value)))} />
-                    <input type="number" onChange={e =>(setDistancia(Number(e.target.value)))} />
+        <main className='pag5'>
+            <div className='agrp1'>
+                <div className='aa'>
+                    <input type="number" value={tanque} onChange={e => setTanque(Number(e.target.value))} />
+                    <input type="number" value={consumo} onChange={e => setConsumo(Number(e.target.value))} />
+                    <input type="number" value={distancia} onChange={e => setDistancia(Number(e.target.value) )} />
+                    <button onClick={Paradas}> Calcular </button>
+                    <p> {resp} </p>
                 </div>
             </div>
         </main>
